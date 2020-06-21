78c20258bb152c243398ea10888f906d7a99e4af
Geronimo Olanda <51466744+GeronimoOlanda@users.noreply.github.com>
2020-06-21 00:09:46 -0300

    Add files via upload

---
+++ vagas.php
@@ -0,0 +1,7 @@
+<?php
+// include inclue elementos de outras paginas
+include('menu.php'); // se der erro exibe um aviso e continua executando o codigo
+    //require('menu.php'); // se der erro exibe um fatal erro e impede a execução do programa
+
+    
+?>
